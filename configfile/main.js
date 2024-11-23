ba1fafc3ce10034b2b3a973b0d4fa87c0f0f07d5
Test_NotionBDD <e.n7design@gmail.com>
2024-11-24 00:59:50 +0100

    Correction animation home section

--- configfile/main.js
+++ configfile/main.js
@@ -73,19 +73,30 @@ $(".Btn-Annonce").click(function(){
     }, 800); // Correspond au temps de l'animation CSS (800ms)
 });
 
+
 // ........................
 // Pour Home Section
 $(".Btn-Home").click(function(){
-  $("#main-content").toggleClass("mainHide");
-  $("#main-content").css("z-index", "500");
+  // $("#main-content").toggleClass("mainHide");
+  $("#main-content").css("position", "absolute"); 
+  $("#main-content").css("height", "0"); 
+
 
   // Reset z-index Autres sections
   $(".CantiqueList-Section").css("z-index", "200");
   $(".Annonce-Section").css("z-index", "200");
 
-  // Attends la fin de l'animation avant de masquer #main-content
+  // En suite animer la section 
   setTimeout(function () {
+    $("#main-content").css("z-index", "500");
+    $("#main-content").css("height", "100dvh");
+  }, 300); // Correspond au temps de l'animation CSS
+
+  // En suite reset la position des autres section et initialiser celle-ci
+  setTimeout(function () {
+    $("#main-content").css("position", "relative");
+    $("#main-content").css("height", "auto"); 
     $(".CantiqueList-Section").removeClass("CantiqueListhide");
     $(".Annonce-Section").removeClass("AnnonceHide");
-  }, 800); // Correspond au temps de l'animation CSS (800ms)
+  }, 700);
 });
